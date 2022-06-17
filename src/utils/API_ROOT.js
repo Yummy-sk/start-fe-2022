94da4859cc1b5fd739d4c85ba2f64fca1992cd78
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-17 13:18:46 +0900

    fix: api 수정

--- src/utils/API_ROOT.js
+++ src/utils/API_ROOT.js
@@ -1,4 +1,4 @@
 export const CLASS_ROOT =
   'https://gist.githubusercontent.com/Yummy-sk/7b92071205c0d4577c531bb668d58492/raw/a4f7332d21defac325cefc1cfb118237bf12cdd0/class';
 export const QUIZ_ROOT =
-  'https://gist.githubusercontent.com/Yummy-sk/b49c309294270417b130a7c9cc2a90c5/raw/57969a2343e20845ac5a1a5892529f3b55717d06/quiz';
+  'https://gist.githubusercontent.com/Yummy-sk/b49c309294270417b130a7c9cc2a90c5/raw/8b0606e125cab6df17043aaff233cde030efc96f/quiz';
